6aad2a9c0c9322add94b037ba6c49f2b67d2fd75
logonrmlocal <valmonty96@gmail.com>
2019-03-14 09:24:14 -0300

    asd

--- app/src/main/java/calculadoradegorjetas/fiap/com/br/calculadoradegorjetas/MainActivity.java
+++ app/src/main/java/calculadoradegorjetas/fiap/com/br/calculadoradegorjetas/MainActivity.java
@@ -2,6 +2,8 @@ package calculadoradegorjetas.fiap.com.br.calculadoradegorjetas;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -28,9 +30,48 @@ public class MainActivity extends AppCompatActivity {
     private NumberFormat percentFormat =
             NumberFormat.getPercentInstance();
 
-    private double billAmount;
+    private double billAmount = 0.0;
 
-    private double percent;
+    private double percent = 0.15;
+
+    private class SeekBarChangeListener implements SeekBar.OnSeekBarChangeListener{
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    }
+
+    private TextWatcher amountEditTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int i, int i1, int i2) {
+            billAmount = Double.parseDouble(s.toString());
+            double tip = billAmount * percent;
+            double total = billAmount + tip;
+            amountTextView.setText(currencyFormat.format(billAmount));
+            tipTextView.setText(currencyFormat.format(tip));
+            totalTextView.setText(currencyFormat.format(total));
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +86,7 @@ public class MainActivity extends AppCompatActivity {
         tipTextView = findViewById(R.id.tipTextView);
         totalTextView = findViewById(R.id.totalTextView);
 
-
+        amountEditText.addTextChangedListener(amountEditTextWatcher);
+        percentSeekBar.setOnSeekBarChangeListener(new SeekBarChangeListener());
     }
 }
